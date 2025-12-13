7ac3af5e904da49f018690eefea1562ef7ae24f4
EgasKrylov <156203775+EgasKrylov@users.noreply.github.com>
2025-12-13 20:29:43 +0300

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,27 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a + b;
     }
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a - b;
     }
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if (b == 0) {
+            System.out.println("Деление на ноль!");
+            return 0;
+        }
+        return a / b;
     }
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a * b;
     }
     public int solver(){
-        //TODO inser your realisation in method solver
+        int a = 2;
+        int b = 5;
+
+        int result = dif(b, a);
+        int x = div(b, result);
+
+        return x;
     }
 }
